79d29ae38fc28762d3454b4ebfb419367462980b
Ushou <93358495+Ushou@users.noreply.github.com>
2022-05-18 13:31:36 +0800

    Update 猜1到10之間一數.java

--- 猜1到10之間一數.java
+++ 猜1到10之間一數.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 public class Main{
 	private static Random ran = new Random();
-	private static int ans = ran.nextInt(10)+1;
+	private static int answer = ran.nextInt(10)+1;
 	public static void main(String args[]) {
 		int en = 0;
-		System.out.print("**The answer is "+String.valueOf(ans));
+		System.out.print("**The answer is "+answer);	//確認答案為何
 		System.out.println(" **");
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please enter a number to guess the answer between 1 to 10");
-		while (en != ans) {
+		while (en != answer) {
 			en = s.nextInt();
-			if(en>10) {
+			if(en>10) {	//輸入的值大於10
 				System.out.println("You can't higher then 10!");
 				continue;
 			}
-			if(en<1) {
+			if(en<1) {	//輸入的值小於1
 				System.out.println("You can't lower then 1!");
 				continue;
 			}
-			if(en>ans) {
+			if(en>answer) {	//輸入的值大於答案
 				System.out.println("Answer is more lower");
 				continue;
 			}
-			else if(en<ans) {
+			else if(en<answer) {	//輸入的值小於答案
 				System.out.println("Answer is more higher");
 				continue;
 			}
-			else {
-				System.out.println("Congratulation! the answer is "+ans);
+			else {	//猜到答案
+				System.out.println("Congratulation! the answer is "+answer);
 				s.close();
 			}
 		}
